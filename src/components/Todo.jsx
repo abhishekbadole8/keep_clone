37356cebe9ef78c6a31e7219372c939fedab02c2
Abhishek Badole <abhishekbadole8@gmail.com>
2023-07-30 13:17:23 +0530

    css update

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,6 +1,6 @@
 import "../styles.css"
 
-function Todo({ todos, editTodo, setEditTodo, handleEdit, handleDelete, handleUpdate, handleComplete, setInputValue,inputValue }) {
+function Todo({ todos, editTodo, setEditTodo, handleEdit, handleDelete, handleUpdate, handleComplete, setInputValue, inputValue }) {
 
     return (
         <>
@@ -15,14 +15,15 @@ function Todo({ todos, editTodo, setEditTodo, handleEdit, handleDelete, handleUp
 
                             <input type="checkbox" className="todo-complete" onClick={() => handleComplete(id)} />
 
-                            {editTodo.id ==id && editTodo.isEditing ?
+                            {editTodo.id == id && editTodo.isEditing ?
                                 <input type="text" className="todo-input-edit" value={inputValue} onChange={(e) => setInputValue(e.target.value)} /> :
-                                <input type="text" className="todo-input" value={content} />}
+                                // <input type="text"  value= />
+                                <p className="todo-input">{content}</p>}
                         </div>
 
                         <div className="buttons">
 
-                            {editTodo.id ==id && editTodo.isEditing?
+                            {editTodo.id == id && editTodo.isEditing ?
                                 <i class="fa-solid fa-upload" onClick={() => handleUpdate(id)}></i > :
                                 <i class="fa-solid fa-pen-to-square" onClick={() => handleEdit(todo)}></i>}
 
